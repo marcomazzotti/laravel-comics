98ec8d62f4cd42cda0e671d9523bb8e26fad539d
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-05-23 16:42:58 +0200

    Add: header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,38 @@
+<nav>
+    <div class="container">
+        <div class="nav-list py-4 ">
+            <ul class="list-unstyled d-flex justify-content-center align-items-center">
+                <li class="me-4">
+                    <a href="{{ url('/characters') }}">CHARACTERS</a>
+                </li>
+                <li class="me-4 active">
+                    <a href="{{ url('/') }}">COMICS</a>
+                </li>
+                <li class="me-4">
+                    <a href="{{ url('/movies') }}">MOVIES</a>
+                </li>
+                <li class="me-4">
+                    <a href="{{ url('/tv') }}">TV</a>
+                </li>
+                <li class="me-4">
+                    <a href="{{ url('/games') }}">GAMES</a>
+                </li>
+                <li class="me-4">
+                    <a href="{{ url('/collectibles') }}">COLLECTIBLES</a>
+                </li>
+                <li class="me-4">
+                    <a href="{{ url('/videos') }}">VIDEOS</a>
+                </li>
+                <li class="me-4">
+                    <a href="{{ url('/fans') }}">FANS</a>
+                </li>
+                <li class="me-4">
+                    <a href="{{ url('/news') }}">NEWS</a>
+                </li>
+                <li>
+                    <a href="{{ url('/shop') }}">SHOP</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/tv.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <h1>Tv</h1>
+    </div>
+@endsection
